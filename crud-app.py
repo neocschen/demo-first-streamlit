4894de3c94b7f65004c47acfff3bf00f519b8f95
neocschen <neo.cs.chen@gmail.com>
2026-03-02 16:31:09 +0800

    Update crud-app.py

--- crud-app.py
+++ crud-app.py
@@ -18,7 +18,7 @@ gc = init_connection()
 # ==========================================
 # 2. 開啟指定的試算表與工作表
 # ==========================================
-SHEET_INPUT = "https://docs.google.com/spreadsheets/d/1tB77kL8O2jLjruSeg96OAzDYJ2aVbULUrxIkQ1dqjKE/edit?gid=0#gid=0"
+SHEET_INPUT = "https://docs.google.com/spreadsheets/d/1tB77kL8O2jLjruSeg96OAzDYJ2aVbULUrxIkQ1dqjKE/edit?usp=sharing"
 WORKSHEET_NAME = "工作表1"
 
 try:
